1e728ed644f54ed4721f7fbe682cc0a157f24f59
Morgan Steckler (6ru) <morganrsteckler@gmail.com>
2024-05-15 17:45:27 -0400

    closes #51: added metadata and reformatted xarrays

--- GFW/convert.py
+++ GFW/convert.py
@@ -37,7 +37,7 @@ target_res = 0.5
 resampled_data = data.coarsen(x=(int(target_res / abs(data.rio.resolution()[0]))),
                               y=(int(target_res / abs(data.rio.resolution()[1])))).mean()
 
-# rename x/y dims to lat/lon
+# rename variable and x/y dims to lat/lon
 ds = resampled_data.rename({'x': 'lon', 'y': 'lat', 'band_1':'c_uptake_rate'})
 
 # numpy array of time bounds
@@ -53,14 +53,32 @@ tb_da = xr.DataArray(tb_arr, dims=('time', 'nv'))
 ds = ds.expand_dims(time=tb_da.mean(dim='nv'))
 ds['time_bounds'] = tb_da
 
-# edit time attributes
-ds['time'].encoding['units'] = 'days since 2020-01-01'
-ds['time'].attrs['bounds'] = 'time_bounds'
+# define attributes
+t_attrs = {
+    'long_name':'time',
+    'units':'days since 2020-01-01',
+    'calendar':'noleap',
+    'bounds':'time_bounds',
+}
+y_attrs = {
+    'long_name':'latitude',
+    'units':'degrees_north',
+}
+x_attrs = {
+    'long_name':'longitude',
+    'units':'degrees_east',
+}
+v_attrs = {
+    'long_name': 'mean aboveground biomass carbon accumulation rate',
+    'units': 'Mg ha-1 yr-1',
+    'ancillary_variables': 'error_ratio',
+}
 
-# edit variable attributes
-ds['c_uptake_rate'].attrs = {
-    'long_name': "mean aboveground biomass carbon accumulation rate",
-    'units': 'Mg ha-1 yr-1'}
+# add variable attributes
+ds['time'].attrs = t_attrs
+ds['lat'].attrs = y_attrs
+ds['lon'].attrs = x_attrs
+ds['c_uptake_rate'].attrs = v_attrs
 
 # edit global attributes
 generate_stamp = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(local_data)))
@@ -68,40 +86,18 @@ ds.attrs = {
     'title':'Carbon accumulation potential from natural forest regrowth in forest and savanna biomes',
     'institution':'Global Forest Watch',
     'source':'Ensemble of 100 random forest models using ground measurements and 66 co-located environmental covariate layers',
-    'history':f"""
-    {download_stamp}: downloaded source from {remote_data};
-    {generate_stamp}: resampled to 0.5 degree resolution and created CF-compliant meta-data in {github_path};""",
+    'history':f"""{download_stamp}: downloaded source from {remote_data}
+{generate_stamp}: resampled to 0.5 degree resolution and created CF-compliant meta-data in {github_path}""",
     'references': """
-    @article{Cook-Patton2020,
-      author  = {Susan C. Cook-Patton, Sara M. Leavitt, David Gibbs, Nancy L. Harris, Kristine Lister, Kristina J. Anderson-Teixeira, Russell D. Briggs, Robin L. Chazdon, Thomas W. Crowther, Peter W. Ellis, Heather P. Griscom, Valentine Herrmann, Karen D. Holl, Richard A. Houghton, Cecilia Larrosa, Guy Lomax, Richard Lucas, Palle Madsen, Yadvinder Malhi, Alain Paquette, John D. Parker, Keryn Paul, Devin Routh, Stephen Roxburgh, Sassan Saatchi, Johan van den Hoogen, Wayne S. Walker, Charlotte E. Wheeler, Stephen A. Wood, Liang Xu & Bronson W. Griscom},
-      title   = {Mapping carbon accumulation potential from global natural forest regrowth},
-      journal = {Nature},
-      year    = {2020},
-      volume  = {585},
-      pages   = {545--550}
-      doi     = {https://doi.org/10.1038/s41586-020-2686-x}}""",
+@article{Cook-Patton2020,
+author  = {Susan C. Cook-Patton, Sara M. Leavitt, David Gibbs, Nancy L. Harris, Kristine Lister, Kristina J. Anderson-Teixeira, Russell D. Briggs, Robin L. Chazdon, Thomas W. Crowther, Peter W. Ellis, Heather P. Griscom, Valentine Herrmann, Karen D. Holl, Richard A. Houghton, Cecilia Larrosa, Guy Lomax, Richard Lucas, Palle Madsen, Yadvinder Malhi, Alain Paquette, John D. Parker, Keryn Paul, Devin Routh, Stephen Roxburgh, Sassan Saatchi, Johan van den Hoogen, Wayne S. Walker, Charlotte E. Wheeler, Stephen A. Wood, Liang Xu & Bronson W. Griscom},
+title   = {Mapping carbon accumulation potential from global natural forest regrowth},
+journal = {Nature},
+year    = {2020},
+volume  = {585},
+pages   = {545--550},
+doi     = {https://doi.org/10.1038/s41586-020-2686-x}}""",
     'comment':''}
 
+# export and encode any vars that need to be encoded
 ds.to_netcdf('cookpatton2020_0.5deg.nc', format="NETCDF4", engine="netcdf4")
-
-# below is copied from anoter script:
-
-# # create header
-# ds.attrs = {
-#     "title": "Global Aboveground and Belowground Biomass Carbon Density Maps for the Year 2010",
-#     "version": "1",
-#     "institutions": "University of Wisconsin-Madison",
-#     "source": "25 datasets spanning woody, grassland, tundra, and cropland cover types, and reporting above and below ground biomass (https://www.nature.com/articles/s41597-020-0444-4/tables/2)",
-#     "history": f"""
-# {download_stamp}: Data requested and downloaded from https://doi.org/10.3334/ORNLDAAC/1763;
-# {time.strftime('%Y-%m-%d')}: Coarsened (mass conserving) to 0.5 degree and converted to ILAMB-ready netcdf files.""",
-#     "references": """
-# @ARTICLE{Spawn2020a,
-#   author = {Spawn, S.A., and H.K. Gibbs},
-#   title = {Global Aboveground and Belowground Biomass Carbon Density Maps for the Year 2010},
-#   journal = {ORNL DAAC},
-#   year = {2020},
-#   doi = {https://doi.org/10.3334/ORNLDAAC/1763}
-# }
-# """,
-# }
